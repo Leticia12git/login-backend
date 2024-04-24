73f76c9bb09c9c5e008b048fb46b5d69495afefd
Leticia Lima <leticia_lele.lima@hotmail.com>
2024-04-23 21:32:55 -0300

    subindo projeto

---
+++ src/README.md
@@ -0,0 +1,3 @@
+### Login - Backend
+
+Este projeto visa criar um sistema de login seguro para uma plataforma web, garantindo a autenticação confiável dos usuários. Utilizaremos tecnologias modernas e práticas recomendadas de segurança para proteger as informações sensíveis dos usuários, como senhas e dados pessoais.
---
+++ src/main/java/com/example/loginbackend/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.loginbackend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+}
